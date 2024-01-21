ccd9fec4b1be612c98fbaf5c30ea278251b17317
Arun Kumar Venkatesan <arunvjava@gmail.com>
2024-01-21 12:47:49 +0530

    Testing

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegistrationComponent},
   { path: 'forget-pwd', component: ForgetPwdComponent},
+  { path: 'test-pwd', component: ForgetPwdComponent},
 ];
 
 @NgModule({
